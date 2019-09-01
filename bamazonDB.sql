f9b9adc2af83205537f73b46b33988164d288a1c
Timits <will4f21@yahoo.com>
2019-09-01 05:07:03 -0700

    adjustment in db code to show table data

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -5,20 +5,20 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-    item_id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(30) NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
-    price DECIMAL(10,2) NULL,
-    stock_quantity INT NULL,
+    item_id INT(4) NOT NULL,
+    product_name VARCHAR(60) NOT NULL,
+    department_name VARCHAR(60) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT(20) NOT NULL,
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity);
-VALUES ("t-shirt", "clothing", 5.20, 12);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity);
+VALUES (001,"t-shirt", "clothing", 5.20, 12);
 
 
 INSERT INTO products (product_name, department_name, price, stock_quantity);
-VALUES ("pants", "clothing", 16.50, 20);
+VALUES (024, "pants", "clothing", 16.50, 20);
 
 SELECT * FROM products;
 
