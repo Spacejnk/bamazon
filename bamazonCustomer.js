697dada525c13bcfa830f5ad36271472b9fe1f86
Timits1 <will4f21@yahoo.com>
2019-09-02 04:23:00 -0700

    testing prompt questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,33 +1,94 @@
-var express = require('express');
+// set var for dependencies
+//var express = require('express');
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-var app = express();
+//var app = express();
 
-app.listen('3306', () => {
-    console.log('Server started on port 3306')
-})
+// app.listen('3306', () => {
+//     console.log('Server started on port 3306')
+// })
+
+// create mysql database connection
 var db = mysql.createConnection({
     host: 'localhost',
 
     user: 'root',
 
-    password: '',
+    // password: '',
 
-    database: 'bamazon_db'
+    // database: 'bamazon_db'
 });
 
-db.connect(function(err){
-    if (err) throw err;
-    console.log('bamazon_db is connected' + db.threadId);
-    //db.end();
-    afterConnection();
-});
+// db.connect(function(err){
+//     if (err) throw err;
+//     console.log('bamazon_db is connected' + db.threadId);
+//     //db.end();
+//     afterConnection();
+// });
 
-function afterConnection(){
-    db.query('SELECT * FROM products', function(err, res) {
-        if(err) throw err;
-        console.log(res);
-        db.end();
-    } )
-}
+// function afterConnection(){
+//     db.query('SELECT * FROM products', function(err, res) {
+//         if(err) throw err;
+//         console.log(res);
+//         db.end();
+//     } )
+// }
+
+// function inquirer for prompt messages
+// The app should then prompt users with two messages.
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+
+
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Which product id would you like to buy?"
+          },
+        {
+            type: "checkbox",
+            name: "name",
+            choices: ["clothing", "kitchen", "bathroom", "outdoor"]
+    
+
+          },
+          {
+            type: "checkbox",
+            name: "products",
+            choices: ['t-shirt', 'pants', 'socks', 'fork', 'knife', 'toothbrush', 'soap', 'hose', 'shovel', 'saw']
+          } , 
+          // choose
+          {
+              type: "checkbox",
+              name: "products",
+              message: "How many units would you like to buy?",
+              choices: [1, 2, 5, 10, 50]
+
+
+          }
+
+    ])
+    .then(function(user) {
+        // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+        if (user.choices === 'pants') {
+          console.log("It's available ");
+          
+        }
+        else {
+          console.log("That's okay ");
+        }
+      });
+    
+
+
+
+
+
+
+
+
+
 
