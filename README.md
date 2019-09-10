60e495a612de6a0f9002d8eccf0f7b889d96336f
Timits1 <will4f21@yahoo.com>
2019-09-10 00:09:25 -0700

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # bamazon
+
+> Welcome to Bamazon! This is an Amazon-like storefront built with MySQL and Node.js. Within the customer portal, you can view the inventory, choose an item you'd like to purchase along with the quantity. Transaction complete! However, if we do not have enough of an item or you select an item that is not available in stock we will be unable to fulfill your purchase, the transaction will not go through.
+
+
+
+##Overview
+
+### Built With 
++ MySQL
++ Node.JS
++ Javascript
+
+### Dependencies
++ cli-table2
++ express
++ inquirer
++ mysql
+
+
+## 
+
+
+#### bamazonCustomer.js Intro
++ Place order
+
+![alt text](bamazonintro.png)
+
+#### bamazonCustomer.js Order
++ Order Completed
+
+![alt text](bamazonorder.png)
+
+#### bamazonCustomer.js Not available
++ Unsuccessful order
+
+![alt text](bamazonnotavail.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,132 +1,122 @@
 // set var for dependencies
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-var Table = require('cli-table2');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table2");
 
 // create mysql database connection
 var db = mysql.createConnection({
-    host: 'localhost',
+  host: "localhost",
 
-    user: 'root',
+  user: "root",
 
-    password: '',
+  password: "",
 
-    database: 'bamazon_db'
+  database: "bamazon_db"
 });
 
 db.connect();
 
-var displayStore = function(){
-    db.query('SELECT * FROM products', function(err, res) {
+var displayStore = function() {
+  db.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-      console.log(' *** ------------------ ***');
-      console.log(' *** Welcome to Bamazon ***');
-      console.log(' *** ------------------ ***');
-      
+    console.log(" *** ------------------ ***");
+    console.log(" *** Welcome to Bamazon ***");
+    console.log(" *** ------------------ ***");
 
-      var table = new Table({
-        head: ['Prod_ID', 'Products', 'Price', 'In_Stock_Amt']
-      , colWidths: [15, 15, 7, 14],
-      colAligns: ['center', 'left', 'center', 'center'],
+    var table = new Table({
+      head: ["Prod_ID", "Products", "Price", "In_Stock_Amt"],
+      colWidths: [15, 15, 7, 14],
+      colAligns: ["center", "left", "center", "center"],
       style: {
-          head: ['aqua'],
-          compact: true
+        head: ["aqua"],
+        compact: true
       }
     });
 
-
     for (var i = 0; i < res.length; i++) {
-        
-        table.push([res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]);
-    
+      table.push([
+        res[i].item_id,
+        res[i].product_name,
+        res[i].price,
+        res[i].stock_quantity
+      ]);
     }
     console.log(table.toString());
     askShopInfo();
-    
-    });
-
+  });
 };
 
-
 function askShopInfo() {
-    
-
-    inquirer
+  inquirer
     .prompt([
-        {
-            name: "itemYouWant",
-            type: "input",
-            message: "Which product id would you like to buy?"
-            
-          } 
+      {
+        name: "itemYouWant",
+        type: "input",
+        message: "Which product id would you like to buy?"
+      }
     ])
 
     .then(function(theItemYouPick) {
-        
-        var yourChoice = theItemYouPick.itemYouWant;
+      var yourChoice = theItemYouPick.itemYouWant;
 
-        db.query("SELECT * FROM products WHERE item_id=?", yourChoice, function(err, res){
-            if (err) throw err;
-            
-            if(res.length === 0) {
-                console.log('Item does not exist, Please enter a correct id from list');
-            
-            askShopInfo();
-            
+      db.query("SELECT * FROM products WHERE item_id=?", yourChoice, function(
+        err,
+        res
+      ) {
+        if (err) throw err;
+
+        if (res.length === 0) {
+          console.log(
+            "Item does not exist, Please enter a correct id from list"
+          );
+
+          askShopInfo();
         } else {
-            inquirer .prompt({
-                name: "quantity",
-                type: "input",
-                message: "How many items would you like to buy?"
+          inquirer
+            .prompt({
+              name: "quantity",
+              type: "input",
+              message: "How many items would you like to buy?"
             })
             .then(function(theItemYouPickQuantity) {
-                var numOfItems = theItemYouPickQuantity.quantity;
-                if (numOfItems > res[0].stock_quantity) {
-                    console.log("Sorry we only have " + res[0].stock_quantity + " items of this selected product");
+              var numOfItems = theItemYouPickQuantity.quantity;
+              if (numOfItems > res[0].stock_quantity) {
+                console.log(
+                  "Sorry we only have " +
+                    res[0].stock_quantity +
+                    " items of this selected product"
+                );
 
-                    askShopInfo();
-                } else {
+                askShopInfo();
+              } else {
+                console.log("--------------");
+                console.log(res[0].product_name + " purchased");
+                console.log(numOfItems + " qty @ $" + res[0].price);
+                console.log("--------------");
+                console.log(
+                  "Total Cost is $" + numOfItems * res[0].price.toFixed(2)
+                );
+                var newQuantity = res[0].stock_quantity - numOfItems;
+
+                db.query(
+                  "UPDATE products SET stock_quantity = " +
+                    newQuantity +
+                    " WHERE item_id = " +
+                    res[0].item_id,
+
+                  function(err) {
+                    if (err) throw err;
                     console.log("--------------");
-                    console.log(res[0].product_name + " purchased");
-                    console.log(numOfItems + " qty @ $" + res[0].price);
-                    console.log("--------------");
-                    console.log("Total Cost is " + numOfItems * res[0]
-                    .price.toFixed(3));
-                    var newQuantity = res[0].stock_quantity - numOfItems;
-
-                    db.query(
-                        "UPDATE products SET stock_quantity = " +
-                        newQuantity + " WHERE item_id = " + res[0].item_id,
-
-                        function(err){
-                            if (err) throw err;
-                            console.log("--------------");
-                            console.log("Your order went through.");
-                            console.log("Thank you.");
-                            db.end();
-                        }
-                    );
-                } 
-                
-                
-            })
+                    console.log("Your order went through.");
+                    console.log("Thank you.");
+                    db.end();
+                  }
+                );
+              }
+            });
         }
-        
-        });
-
+      });
     });
-
-};
+}
 
 displayStore();
-
-
-
-
-
-
-
-
-
-
-
